3995f6bd3f64d34c2376deb604d1f8a42077c8e8
VerteDinde <vertedinde@electronjs.org>
2022-01-17 17:48:50 -0800

    build: clean up script

--- script/prepare-appveyor.js
+++ script/prepare-appveyor.js
@@ -5,20 +5,33 @@ const fs = require('fs');
 const got = require('got');
 const path = require('path');
 
-// Appveyor image constants
-// https://ci.appveyor.com/api/build-clouds/{buildCloudId}
+// Appveyor image constants | https://ci.appveyor.com/api/build-clouds/{buildCloudId}
 const APPREVYOR_IMAGES_URL = 'https://ci.appveyor.com/api/build-clouds'; // GET
 const BAKE_APPVEYOR_IMAGE_URL = 'https://ci.appveyor.com/api/builds'; // POST
 const USE_APPVEYOR_IMAGE_URL = 'https://ci.appveyor.com/api/builds'; // POST
 
-async function checkAppVeyorImage (opts) {
-  // check the image for the available chromium number
-  // eslint-disable-next-line no-control-regex
-  const versionRegex = new RegExp('chromium_version\':\n +\'(.+?)\',', 'm');
-  const deps = fs.readFileSync(path.resolve(__dirname, '../DEPS'), 'utf8');
-  const [, CHROMIUM_VERSION] = versionRegex.exec(deps);
+async function makeRequest({ auth, url, headers, body, method }) {
+  const clonedHeaders = {
+    ...(headers || {})
+  };
+  if (auth && auth.bearer) {
+    clonedHeaders.Authorization = `Bearer ${auth.bearer}`;
+  }
+  const response = await got(url, {
+    headers: clonedHeaders,
+    body,
+    method,
+    auth: auth && (auth.username || auth.password) ? `${auth.username}:${auth.password}` : undefined
+  });
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    console.error('Error: ', `(status ${response.statusCode})`, response.body);
+    throw new Error(`Unexpected status code ${response.statusCode} from ${url}`);
+  }
+  return JSON.parse(response.body);
+}
 
-  const IMAGE_URL = `${APPREVYOR_IMAGES_URL}/${opts.buildCloudId}`;
+async function checkAppVeyorImage(options) {
+  const IMAGE_URL = `${APPREVYOR_IMAGES_URL}/${options.buildCloudId}`;
   const requestOpts = {
     url: IMAGE_URL,
     auth: {
@@ -31,27 +44,18 @@ async function checkAppVeyorImage (opts) {
   };
 
   try {
-    // const res = await makeRequest(requestOpts, true);
     const { settings } = await makeRequest(requestOpts, true);
     const { cloudSettings } = settings;
-    console.log('cloudSettings: ', cloudSettings);
-
-    const version = cloudSettings.images.find(image => image.name === `${CHROMIUM_VERSION}`) || null;
-    if (version) {
-      console.log(`Image exists for ${CHROMIUM_VERSION}. Continuing AppVeyor jobs using ${opts.buildCloudId}`);
-    } else {
-      console.log(`No AppVeyor image found for ${CHROMIUM_VERSION} in ${opts.buildCloudId}. Creating new image...`);
-      await bakeAppVeyorImage({ ...opts, version: CHROMIUM_VERSION });
-      // should we continue with an older cached image here?
-      // await useAppVeyorImage(opts);
-    }
+    return cloudSettings.images.find(image => image.name === `${options.imageVersion}`) || null;
   } catch (err) {
     console.log('Could not call AppVeyor: ', err);
   }
 }
 
-async function bakeAppVeyorImage (options) {
-  console.log(`Triggering a new AppVeyor image for: ${options.version} on build cloud: ${options.buildCloudId}...`);
+async function bakeAppVeyorImage(options) {
+  console.log(`Baking a new AppVeyor image for: ${options.version}, on build cloud ${options.buildCloudId}...`);
+  return;
+
   const environmentVariables = {
     ELECTRON_RELEASE: 0,
     APPVEYOR_BUILD_WORKER_CLOUD: options.buildCloudId,
@@ -79,7 +83,6 @@ async function bakeAppVeyorImage (options) {
     }),
     method: 'POST'
   };
-  // jobRequestedCount++;
 
   try {
     const { version } = await makeRequest(requestOpts, true);
@@ -91,17 +94,13 @@ async function bakeAppVeyorImage (options) {
 }
 
 // TODO: We'll need to replace the webhook that calls current AppVeyor builds with a call to this function
-async function useAppVeyorImage (targetBranch, job, options) {
-  console.log(`Triggering AppVeyor to run build job: ${job} on branch: ${targetBranch} with release flag.`);
+async function useAppVeyorImage(options) {
+  console.log(`Using AppVeyor image ${options.version} on build cloud ${options.buildCloudId}...`);
   const environmentVariables = {
     ELECTRON_RELEASE: 1,
     APPVEYOR_BUILD_WORKER_CLOUD: options.buildCloudId,
     APPVEYOR_BUILD_WORKER_IMAGE: options.version
   };
-
-  if (!options.ghRelease) {
-    environmentVariables.UPLOAD_TO_S3 = 1;
-  }
 
   const requestOpts = {
     url: USE_APPVEYOR_IMAGE_URL,
@@ -120,7 +119,6 @@ async function useAppVeyorImage (targetBranch, job, options) {
     }),
     method: 'POST'
   };
-  // jobRequestedCount++;
 
   try {
     const { version } = await makeRequest(requestOpts, true);
@@ -131,25 +129,42 @@ async function useAppVeyorImage (targetBranch, job, options) {
   }
 }
 
-// Helpers
-async function makeRequest ({ auth, url, headers, body, method }) {
-  const clonedHeaders = {
-    ...(headers || {})
-  };
-  if (auth && auth.bearer) {
-    clonedHeaders.Authorization = `Bearer ${auth.bearer}`;
+async function prepareAppVeyorImage(targetBranch, opts) {
+  // eslint-disable-next-line no-control-regex
+  const versionRegex = new RegExp('chromium_version\':\n +\'(.+?)\',', 'm');
+  const deps = fs.readFileSync(path.resolve(__dirname, '../DEPS'), 'utf8');
+  const [, CHROMIUM_VERSION] = versionRegex.exec(deps);
+
+  const imageVersion = opts.imageVersion || CHROMIUM_VERSION;
+  const image = await checkAppVeyorImage({ buildCloudId: opts.buildCloudId, imageVersion });
+
+  if (image) {
+    console.log(`Image exists for ${image}. Continuing AppVeyor jobs using ${opts.buildCloudId}`);
+    await useAppVeyorImage({ ...opts, version: image });
+  } else {
+    console.log(`No AppVeyor image found for ${imageVersion} in ${opts.buildCloudId}.`);
+    console.log(`Creating new image for ${imageVersion} and falling back to default image in ${opts.buildCloudId}.`);
+    // await bakeAppVeyorImage({ ...opts, version: CHROMIUM_VERSION });
+    // continue with an existing image
+    // await useAppVeyorImage({ ...opts });
   }
-  const response = await got(url, {
-    headers: clonedHeaders,
-    body,
-    method,
-    auth: auth && (auth.username || auth.password) ? `${auth.username}:${auth.password}` : undefined
-  });
-  if (response.statusCode < 200 || response.statusCode >= 300) {
-    console.error('Error: ', `(status ${response.statusCode})`, response.body);
-    throw new Error(`Unexpected status code ${response.statusCode} from ${url}`);
-  }
-  return JSON.parse(response.body);
 }
 
-checkAppVeyorImage({ buildCloudId: '682', version: 'vs2019bt-16.4.0' });
+module.exports = prepareAppVeyorImage;
+
+if (require.main === module) {
+  const args = require('minimist')(process.argv.slice(2));
+  console.log('ARGS: ', args)
+  const targetBranch = args._[0];
+  // if (args._.length < 1) {
+  //   console.log(`Load or bake AppVeyor images for Windows CI.
+  //   Usage: prepare-appveyor.js [--buildCloudId=CLOUD_ID] [--appveyorJobId=xxx] [--commit=sha] TARGET_BRANCH
+  //   `);
+  //   process.exit(0);
+  // }
+  prepareAppVeyorImage(targetBranch, args)
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+}
